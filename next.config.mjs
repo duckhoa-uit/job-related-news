a8fc726a8d07701ec1e61a9756944492d099d38a
DeeNKay <duckhoa.dev@gmail.com>
2023-03-31 12:03:52 +0700

    ignore type check on build

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'res.cloudinary.com'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
 }
 
 const withMDX = nextMDX({
